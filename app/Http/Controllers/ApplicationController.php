3e39be74eb03057fce811b659828a06e8641085f
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-12-25 11:10:11 +0600

    added application store code

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Application;
+use App\ApplicationFee;
+use App\Field;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -15,7 +17,29 @@ class ApplicationController extends Controller {
 	}
 
 	public function store(Request $request) {
-		//
+        $validated = $request->validate([
+            'category_id' => 'required|numeric',
+            'date' => 'required',
+            'amount' => 'required|numeric',
+            'fields' => 'required|array' // field_id, optional_field_name, unit, amount
+        ]);
+
+        $application = Application::create([
+            'category_id' => $validated['category_id'],
+            'date' => Carbon::parse($validated['date']),
+            'amount' => $validated['amount']
+        ]);
+
+        foreach ($validated['fields'] as $fieldInfo){
+            $field = (is_numeric($fieldInfo['field_id']))? Field::find($fieldInfo['field_id']): null;
+            ApplicationFee::create([
+                'application_id' => $application->id,
+                'field_id' => ($field? $field->id: null),
+                'optional_field_name' => ($field? '': $fieldInfo['optional_field_name']),
+                'unit' => ($field? $field->unit: $fieldInfo['unit']),
+                'amount' => ($field? $field->amount: $fieldInfo['amount'])
+            ]);
+        }
 	}
 
 	public function show($id) {
