c7ce318811de214b13cb440911b6e0d4e8490917
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-12-26 01:10:57 +0600

    application basic form input done ui

--- resources/js/pages/CreateApplication.vue
+++ resources/js/pages/CreateApplication.vue
@@ -5,11 +5,11 @@
             class="q-gutter-md"
         >
             <div class="row">
-                <q-input class="col-md-3 col-xs-12 q-pr-md" filled v-model="applicationForm.date" mask="date" :rules="['date']" label="Date">
+                <q-input class="col-md-3 col-xs-12 q-pr-md q-pb-md" filled v-model="applicationForm.date"  label="Date">
                     <template v-slot:append>
                         <q-icon name="event" class="cursor-pointer">
                             <q-popup-proxy ref="qDateProxy" cover transition-show="scale" transition-hide="scale">
-                                <q-date v-model="applicationForm.date">
+                                <q-date v-model="applicationForm.date" today-btn mask="DD/MM/YYYY">
                                     <div class="row items-center justify-end">
                                         <q-btn v-close-popup label="Close" color="primary" flat />
                                     </div>
@@ -19,7 +19,13 @@
                     </template>
                 </q-input>
 
-                <q-select class="col-md-4 col-xs-12 q-pr-md q-pb-md" filled v-model="applicationForm.category_id" :options="categories" label="Category" />
+                <q-select class="col-md-4 col-xs-12 q-pr-md q-pb-md" filled v-model="applicationForm.category_id"
+                          :options="categories"
+                          option-label="name"
+                          option-value="id"
+                          map-options
+                          emit-value
+                          label="Category" />
 
                 <q-input
                     class="col-md-3 col-xs-12 q-pr-md"
@@ -45,11 +51,14 @@
 </template>
 
 <script>
+import { Notify } from 'quasar'
+import { date } from 'quasar'
+
 
 const generateApplicationTemplate = () => {
     return {
         category_id: '',
-        date: new Date(),
+        date: date.formatDate(Date.now(), 'DD/MM/YYYY'),
         amount: '',
         fields: []
     }
@@ -68,10 +77,21 @@ export default {
     },
     methods: {
         loadCategories() {
-
+            this.$axios.get('/api/categories')
+            .then((res) => {
+                this.categories = res.data
+            })
         },
         storeApplication() {
+            this.$axios.post('/api/applications', this.applicationForm)
+            .then((res) => {
+                Notify.create({
+                    message: 'Application Created Successfully!'
+                })
 
+                this.applicationForm = generateApplicationTemplate()
+                scrollTo(0, 0)
+            })
         }
     }
 }
